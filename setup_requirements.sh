806dbef6261b33abec182c298ce13a4981c089a3
vecna <vecna@globaleaks.org>
2014-05-27 23:19:17 +0200

    added the other requirements

--- setup_requirements.sh
+++ setup_requirements.sh
@@ -2,4 +2,5 @@
 # I'm puttint the "-x" because there are some 'sudo'...
 
 sudo aptitude update
-sudo aptitude install phantomjs
+sudo aptitude install phantomjs traceroute python-pip
+sudo pip install GeoIP
