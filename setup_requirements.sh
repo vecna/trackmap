14b1949f1ead3106e7281426caa1fa0d9bc69d4a
vecna <vecna@globaleaks.org>
2014-05-30 17:58:40 +0200

    completed requirements

--- setup_requirements.sh
+++ setup_requirements.sh
@@ -2,5 +2,5 @@
 # I'm puttint the "-x" because there are some 'sudo'...
 
 sudo aptitude update
-sudo aptitude install phantomjs traceroute python-pip
+sudo aptitude install phantomjs traceroute python-pip gcc python-pip python-dev libgeoip-dev
 sudo pip install GeoIP tldextract
