1326dcec526c71f9828e63d4fcf9e3898eb59e4b
vecna <vecna@globaleaks.org>
2014-05-31 14:18:01 +0200

    added a note about phantomjs <= 1.4

--- setup_requirements.sh
+++ setup_requirements.sh
@@ -4,3 +4,12 @@
 sudo aptitude update
 sudo aptitude install phantomjs traceroute python-pip gcc python-pip python-dev libgeoip-dev
 sudo pip install GeoIP tldextract
+
+# remind: handle 
+# phantomjs -h 
+# 2014-05-31T10:35:30 [WARNING] phantomjs: cannot connect to X server 
+# In this case, your distribution has phantom 1.5 and this is bad
+# you've to download
+# http://phantomjs.googlecode.com/files/phantomjs-1.8.1-linux-x86_64.tar.bz2
+# and put the binary in the execution path (simply replace the origianl phantomjs)
+
