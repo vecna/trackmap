e443862b9d4a819d55a0e1be56422150289b43d0
vecna <vecna@globaleaks.org>
2014-07-31 12:47:33 +0200

    added libtrackmap to handle shared codes between scripts

--- perform_analysis.py
+++ perform_analysis.py
@@ -6,6 +6,7 @@ import tldextract
 
 from subprocess import Popen, PIPE
 from termcolor import colored
+from libtrackmap import sortify, media_file_cleanings
 
 OUTPUTDIR = 'output'
 PERMITTED_SECTIONS = [ 'global', 'national', 'local', 'blog' ]
@@ -88,23 +89,6 @@ def do_phantomjs(url, destfile, media_kind):
         print colored("Correctly fetch %d URLs - %s media" % (included_url_number, media_kind), "green")
 
 
-def get_unique_urls(source_urldir, urldumpsf):
-    urls = {}
-    with file(urldumpsf) as f:
-        for url_request in f.readlines():
-            if url_request.startswith('http://'):
-                urls[ url_request[7:].split('/')[0] ] = True
-            elif url_request.startswith('https://'):
-                urls[ url_request[8:].split('/')[0] ] = True
-            elif url_request.startswith('data:'):
-                continue
-            else:
-                print "![ Unexpected link format!", url_request, "from", source_urldir, "]!"
-                continue
-
-    return urls.keys()
-
-
 def do_trace(dumpprefix, host):
 
     ip_file = os.path.join(OUTPUTDIR, '_traceroutes', "%s_ip.json" % dumpprefix)
@@ -169,163 +153,6 @@ def do_trace(dumpprefix, host):
         json.dump(country_travel_path, f)
 
     print colored("* = %d" % asterisks_total, 'red')
-
-
-
-def sortify(outputdir):
-
-    urldict = {}
-    skipped = 0
-
-    for urldir in os.listdir(outputdir):
-
-        if urldir in ['phantom.log', '_traceroutes', 'unique_id',
-                      '_verbotracelogs', 'domain.infos', 'country']:
-            continue
-
-        try:
-            urlfile = os.path.join(outputdir, urldir, '__urls')
-            related_urls = get_unique_urls(urldir, urlfile)
-        except IOError or OSError as einfo:
-            print "Unable to read", urldir, einfo, "skipping"
-            continue
-
-        for url in related_urls:
-
-            if urldict.has_key(url):
-                skipped +=1
-                continue
-
-            dnsquery = tldextract.extract(url)
-            urldict.update({url : {
-                    'domain' : dnsquery.domain,
-                    'tld' : dnsquery.suffix,
-                    'subdomain' : dnsquery.subdomain }
-                })
-
-        # note: 
-        # https://raw.github.com/mozilla/gecko-dev/master/netwerk/dns/effective_tld_names.dat
-        # tldextract is based on this file, and cloudfront.net is readed as TLD. but is fine
-        # I've only to sum domain + TLD in order to identify the "included entity"
-
-    # just to know if the optimization is working well :)
-    print "multiple entry on", skipped,
-    return urldict
-
-def url_cleaner(line):
-
-    # cleanurl is used to create the dir, media to phantomjs
-    if line.startswith('http://'):
-        cleanurl = line[7:]
-    elif line.startswith('https://'):
-        cleanurl = line[8:]
-        print "https will be converted in http =>", line
-    else:
-        raise Exception("Invalid protocol in: %s" % line)
-
-    while cleanurl[-1] == '/':
-        cleanurl = cleanurl[:-1]
-
-    dirtyoptions = cleanurl.find("?")
-    if dirtyoptions != -1:
-        cleanurl = cleanurl[:dirtyoptions]
-
-    cleanurl = cleanurl.split('/')[0]
-    return cleanurl
-
-
-def load_global_file(GLOBAL_MEDIA_FILE):
-
-    global_media_dict = {}
-    counter = 0
-
-    with file(GLOBAL_MEDIA_FILE, 'r') as f:
-        for line in f.readlines():
-
-            line = line[:-1]
-
-            if len(line) > 1 and line[0] == '#':
-                continue
-
-            # everything after a 0x20 need to be cut off
-            line = line.split(' ')[0]
-
-            if len(line) < 3:
-                continue
-
-            cleanurl = url_cleaner(line)
-            counter += 1
-            global_media_dict.update({ cleanurl : 'global' })
-
-    return global_media_dict, counter
-
-
-GLOBAL_MEDIA_FILE = 'special_media/global'
-def media_file_cleanings(linelist, globalfile=GLOBAL_MEDIA_FILE):
-    """
-    From the format
-    [global]
-    http://url
-    # comment
-    [othersec]
-    http://otherweb
-
-    return { 'url': 'global', 'otherweb': 'othersec' }
-    """
-    retdict = {}
-    current_section = None
-    counter_section = 0
-
-    for line in linelist:
-
-        line = line[:-1]
-
-        if len(line) > 1 and line[0] == '#':
-            continue
-
-        # everything after a 0x20 need to be cut off
-        line = line.split(' ')[0]
-
-        if len(line) < 3:
-            continue
-
-        if line.startswith('[') and line.find(']') != -1:
-            candidate_section = line[1:-1]
-
-            if not candidate_section in PERMITTED_SECTIONS:
-                print "The section in", line, "is invalid: do not match with", PERMITTED_SECTIONS
-                quit(-1)
-
-            # if we hot 'global' section: is special!
-            if candidate_section == 'global':
-                retdict, counter_section = load_global_file(globalfile)
-                print "Global file loaded, with # entries", counter_section
-                continue
-
-            if current_section:
-                print "Section", current_section, "has got # entries", counter_section
-                counter_section = 0
-
-            current_section = candidate_section
-            continue
-
-        cleanurl = url_cleaner(line)
-
-        if not current_section:
-            print "detected URL", cleanurl, "without a section! (old file format ?"
-            quit(-1)
-
-        if retdict.has_key(cleanurl):
-            print "Note:", cleanurl, "is duplicated"
-
-        retdict.update({ cleanurl: current_section })
-        counter_section += 1
-
-    # the last section is printed here
-    if current_section:
-        print "Section", current_section, "has got # entries", counter_section
-
-    return retdict
 
 
 def main():
