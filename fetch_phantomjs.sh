eee242fd930d2fe9561091b9ce878778da9f7ee7
vecna <vecna@globaleaks.org>
2014-09-22 11:58:06 +0200

    updated fetch_phantomjs to bypass googlecode

--- fetch_phantomjs.sh
+++ fetch_phantomjs.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 ARCH=$(uname -m)
+# i686 or x86_64 supported only
 
 PHANTOMJS_VER=1.9.2
 PHANTOMJS=phantomjs-$PHANTOMJS_VER-linux-$ARCH
 PHANTOMJS_BIN=phantom-$PHANTOMJS_VER
-PHANTOMJS_DIST=$PHANTOMJS.tar.bz2
-PHANTOMJS_URL=https://phantomjs.googlecode.com/files/$PHANTOMJS_DIST
+PHANTOMJS_DIST=$PHANTOMJS.tar.bz2?raw=true
+
+PHANTOMJS_URL=https://github.com/vecna/phantomjs-mirror/blob/master/$PHANTOMJS_DIST
 
 wget -c $PHANTOMJS_URL
 if [ ! -d $PHANTOMJS ] ; then
-    tar xf $PHANTOMJS_DIST
+    mv $PHANTOMJS_DIST $PHANTOMJS.tar.bz2
+    tar xf $PHANTOMJS.tar.bz2
     ln -s $PHANTOMJS/bin/phantomjs $PHANTOMJS_BIN
 fi
