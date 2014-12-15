fe45b99152b313cdcadb41cfbc72f471cbcfd21a
vecna <vecna@globaleaks.org>
2014-12-15 11:03:09 +0100

    updated Phantom JS to 1.9.8

--- fetch_phantomjs.sh
+++ fetch_phantomjs.sh
@@ -3,11 +3,15 @@
 ARCH=$(uname -m)
 # i686 or x86_64 supported only
 
-PHANTOMJS_VER=1.9.2
+PHANTOMJS_VER=1.9.8
 PHANTOMJS=phantomjs-$PHANTOMJS_VER-linux-$ARCH
 PHANTOMJS_BIN=phantom-$PHANTOMJS_VER
 PHANTOMJS_DIST=$PHANTOMJS.tar.bz2
-PHANTOMJS_URL=https://github.com/vecna/phantomjs-mirror/raw/master/$PHANTOMJS_DIST
+PHANTOMJS_URL=https://bitbucket.org/ariya/phantomjs/downloads/$PHANTOMJS_DIST
+
+
+# phantomjs-1.9.8-linux-x86_64.tar.bz2
+
 
 wget -c $PHANTOMJS_URL
 if [ ! -d $PHANTOMJS ] ; then
